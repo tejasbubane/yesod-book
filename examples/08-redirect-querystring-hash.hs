008ec57133dba15862a2cd095d873ad869a958ec
Tejas Bubane <tejasbubane@gmail.com>
2019-02-07 08:59:48 +0530

    Add example for redirects
    Also showing querystring params and hash

---
+++ examples/08-redirect-querystring-hash.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Yesod
+import Data.Text (Text)
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+/link1 Link1R GET
+/link2 Link2R GET
+/link3 Link3R GET
+/link4 Link4R GET
+|]
+
+instance Yesod App
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+  setTitle "Redirects"
+  [whamlet|
+    <p>
+      <a href=@{Link1R}>Click to start the redirect chain!
+  |]
+
+getLink1R, getLink2R, getLink3R :: Handler () -- multiple declarations
+getLink1R = redirect Link2R -- /link2
+getLink2R = redirect (Link3R, [("foo", "bar")]) -- /link3?foo=bar
+getLink3R = redirect $ Link4R :#: ("baz" :: Text) -- /link4#baz
+
+getLink4R :: Handler Html
+getLink4R = defaultLayout [whamlet|<p> You made it! |]
+
+main :: IO ()
+main = warp 3000 App
